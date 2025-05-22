1bd45026882f2e6e3cdab7408b31888bceff4379
Muzard Geoffrey <45556519+MuzardGeoffrey@users.noreply.github.com>
2025-05-22 11:46:35 +0200

    évolution de la BDD

--- FamilyTools.EasyCompta/DataBase/Migrations/20250408164220_V0.04_UpdateMultiTable.Designer.cs
+++ FamilyTools.EasyCompta/DataBase/Migrations/20250408164220_V0.04_UpdateMultiTable.Designer.cs
@@ -4,6 +4,7 @@ using FamilyTools.EasyCompta.DataBase.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FamilyTools.EasyCompta.DataBase.Migrations
 {
     [DbContext(typeof(AccountContext))]
-    partial class AccountContextModelSnapshot : ModelSnapshot
+    [Migration("20250408164220_V0.04_UpdateMultiTable")]
+    partial class V004_UpdateMultiTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
